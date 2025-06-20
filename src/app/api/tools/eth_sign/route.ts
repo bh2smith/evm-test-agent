376f10d7ff720a932968c105daa8440fc4a51fe3
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-06-20 13:21:20 +0200

    Fix return field

--- src/app/api/tools/eth_sign/route.ts
+++ src/app/api/tools/eth_sign/route.ts
@@ -26,7 +26,7 @@ export async function GET(request: Request) {
 
     return NextResponse.json(
       {
-        evmSignRequest: {
+        transaction: {
           method: "eth_sign",
           params: [evmAddress, messageHex],
         },
