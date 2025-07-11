8ccb09085677dc41b49a95759ca63a8091b88966
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-11 09:40:02 +0200

    Bun Update Deps (zod v4)

--- src/app/api/tools/sendTransaction/route.ts
+++ src/app/api/tools/sendTransaction/route.ts
@@ -6,14 +6,15 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     console.log("sendTransaction/", searchParams);
-    const {
-      numFail,
-      numSuccess,
-      evmAddress: self,
-    } = SendTransactionSchema.parse(Object.fromEntries(searchParams.entries()));
-    return NextResponse.json(buildSendTransactions(self, numSuccess, numFail), {
-      status: 200,
-    });
+    const { numFail, numSuccess, evmAddress } = SendTransactionSchema.parse(
+      Object.fromEntries(searchParams.entries()),
+    );
+    return NextResponse.json(
+      buildSendTransactions(evmAddress, numSuccess, numFail),
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
     const publicMessage = "Error generating EVM transaction:";
     console.error(publicMessage, error);
