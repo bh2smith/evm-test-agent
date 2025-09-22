155a250480ef2b1d1b01d759927df77c5fb55718
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-22 15:58:48 +0200

    Remove Vercel URL

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,22 @@
-import { DEPLOYMENT_URL } from "vercel-url";
+// import { DEPLOYMENT_URL } from "vercel-url";
+
+// // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
+// const PLUGIN_URL =
+//   DEPLOYMENT_URL ||
+//   `${process.env.NEXT_PUBLIC_HOST || "localhost"}:${process.env.PORT || 3000}`;
+
+// if (!PLUGIN_URL) {
+//   console.error(
+//     "!!! Plugin URL not found in env, BITTE_CONFIG or DEPLOYMENT_URL !!!",
+//   );
+//   process.exit(1);
+// }
 
 const ACCOUNT_ID = process.env.ACCOUNT_ID;
 
-// Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
-const PLUGIN_URL =
-  DEPLOYMENT_URL ||
-  `${process.env.NEXT_PUBLIC_HOST || "localhost"}:${process.env.PORT || 3000}`;
+const bitteConfig = JSON.parse(process.env.BITTE_CONFIG || "{}");
 
-if (!PLUGIN_URL) {
-  console.error(
-    "!!! Plugin URL not found in env, BITTE_CONFIG or DEPLOYMENT_URL !!!",
-  );
-  process.exit(1);
-}
+const PLUGIN_URL = bitteConfig.url || "https://evm-test-agent.vercel.app/";
 
 const SEPOLIA_CHAIN_ID = 11155111;
 
